1791c1b7d81b8a4e5776f558ecba82d5f1eee5d9
Sean Laurent <organicveggie@gmail.com>
2020-10-29 13:38:23 -0700

    Update lock-manager to 0.0.35

--- custom_components/lock-manager/sensor.py
+++ custom_components/lock-manager/sensor.py
@@ -74,11 +74,12 @@ class CodeSlotsData:
                             str(value.value),
                         )
                         # do not update if the code contains *s
+                        code = value.value
                         if "*" in str(value.value):
                             _LOGGER.debug("DEBUG: Ignoring code slot with * in value.")
-                            continue
+                            code = "Unknown"
                         sensor_name = f"code_slot_{value.index}"
-                        data[sensor_name] = value.value
+                        data[sensor_name] = code
 
                 self._data = data
 
@@ -98,11 +99,12 @@ class CodeSlotsData:
                         str(value.value),
                     )
                     # do not update if the code contains *s
+                    code = value.value
                     if "*" in str(value.value):
                         _LOGGER.debug("DEBUG: Ignoring code slot with * in value.")
-                        continue
+                        code = "Unknown"
                     sensor_name = f"code_slot_{value.index}"
-                    data[sensor_name] = value.value
+                    data[sensor_name] = code
 
                 self._data = data
 
@@ -110,7 +112,14 @@ class CodeSlotsData:
         data = None
         test = self._hass.states.get(self._entity_id)
         if test is not None:
-            data = test.attributes["node_id"]
+            try:
+                data = test.attributes["node_id"]
+            except Exception as err:
+                _LOGGER.error(
+                    "Error acquiring node id from entity %s: %s",
+                    self._entity_id,
+                    str(err),
+                )
 
         return data
 
@@ -131,7 +140,7 @@ class CodesSensor(Entity):
     @property
     def unique_id(self):
         """Return a unique, Home Assistant friendly identifier for this entity."""
-        return f"{self._name}_{self._unique_id}"
+        return f"{self._lock_name}_{self._name}_{self._unique_id}"
 
     @property
     def name(self):
@@ -164,4 +173,9 @@ class CodesSensor(Entity):
         # Using a dict to send the data back
 
         if self.data._data is not None:
-            self._state = self.data._data[self._name]
+            try:
+                self._state = self.data._data[self._name]
+            except Exception as err:
+                _LOGGER.warning(
+                    "Code slot %s had no value: %s", str(self._name), str(err)
+                )
